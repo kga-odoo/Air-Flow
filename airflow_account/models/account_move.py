a5ccb76269d6beb240c42704ec041428b51a60a2
Jigar A. (Odoo) <jam@odoo.com>
2019-06-11 08:43:49 -0700

    [FIX][2007496] airflow_account: Added condition to avoid traceback
    Co-authourd-by: Parth Mehta <pme@odoo.com>

--- airflow_account/models/account_move.py
+++ airflow_account/models/account_move.py
@@ -13,34 +13,38 @@ class AccountMove(models.Model):
 
     @api.multi
     def post(self):
+
         invoice = self._context.get('invoice', False)
 
-        if invoice and invoice.move_name and invoice.move_name != '/':
-            new_name = invoice.move_name
-        else:
-            # invoice has types:
-            # 'out_invoice' -> regular customer invoice
-            # 'out_refund' -> customer credit notes
-            new_name = ''
+        if invoice:
+            if invoice.move_name and invoice.move_name != '/':
+                new_name = invoice.move_name
+            else:
+                # invoice has types:
+                # 'out_invoice' -> regular customer invoice
+                # 'out_refund' -> customer credit notes
+                new_name = ''
 
-            if invoice.type == 'out_invoice' and invoice.origin:
-                order_id = self._get_invoice_order(invoice)
-                if order_id:
-                    order_id.write({'number_of_invoices': order_id.number_of_invoices + 1})
-                    seq = order_id.number_of_invoices
-                    new_name = invoice.origin[2:] + '{0:02d}'.format(seq) if invoice.origin.startswith('SO') \
-                        else invoice.origin + '{0:02d}'.format(seq)
+                if invoice.type == 'out_invoice' and invoice.origin:
+                    order_id = self._get_invoice_order(invoice)
+                    if order_id:
+                        order_id.write({'number_of_invoices': order_id.number_of_invoices + 1})
+                        seq = order_id.number_of_invoices
+                        new_name = invoice.origin[2:] + '{0:02d}'.format(seq) if invoice.origin.startswith('SO') \
+                            else invoice.origin + '{0:02d}'.format(seq)
 
-            if invoice.type == 'out_refund' and invoice.refund_invoice_id and invoice.refund_invoice_id.number:
-                order_id = self._get_invoice_order(invoice.refund_invoice_id)
-                if order_id:
-                    order_id.write({'number_of_credit_notes': order_id.number_of_credit_notes + 1})
-                    seq = order_id.number_of_credit_notes
-                    new_name = invoice.refund_invoice_id.number[:-2] + 'CM{0:01d}'.format(seq)
+                if invoice.type == 'out_refund' and invoice.refund_invoice_id and invoice.refund_invoice_id.number:
+                    order_id = self._get_invoice_order(invoice.refund_invoice_id)
+                    if order_id:
+                        order_id.write({'number_of_credit_notes': order_id.number_of_credit_notes + 1})
+                        seq = order_id.number_of_credit_notes
+                        new_name = invoice.refund_invoice_id.number[:-2] + 'CM{0:01d}'.format(seq)
 
-            if new_name:
-                # not sure why we need a loop here... just following the convention
-                for move in self:
-                    move.name = new_name
+                if new_name:
+                    # not sure why we need a loop here... just following the convention
+                    for move in self:
+                        move.name = new_name
+
         res = super(AccountMove, self).post()
+
         return res

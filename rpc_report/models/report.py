9b064f2cef11caa94be8b5153fd58cb3b169568c
ehe <ehe@odoo.com>
2019-08-19 20:51:39 +0000

    [REM] force session store as it seems to be not working atm, need to double check

--- rpc_report/models/report.py
+++ rpc_report/models/report.py
@@ -6,20 +6,20 @@ from odoo.http import request, root
 class RPCReport(models.Model):
     _inherit = 'ir.actions.report'
 
-    def _force_session_store(self):
-        """
-            Authenticate like a normal user to have all the needed info
-            in the session and save the session on the filesystem prior to call
-            wkhtmltopdf
-        """
-        password = request.params['args'][2]
-        db = request.params['args'][0]
-        request.session.authenticate(db, self.env.user.login, password)
-        root.session_store.save(request.session)
+    # def _force_session_store(self):
+    #     """
+    #         Authenticate like a normal user to have all the needed info
+    #         in the session and save the session on the filesystem prior to call
+    #         wkhtmltopdf
+    #     """
+    #     password = request.params['args'][2]
+    #     db = request.params['args'][0]
+    #     request.session.authenticate(db, self.env.user.login, password)
+    #     root.session_store.save(request.session)
 
     @api.multi
     def render_rpc(self, res_ids, data):
-        self._force_session_store()
+        # self._force_session_store()
         res = self.render(res_ids, data)
         if isinstance(res, tuple):
             return base64.b64encode(res[0]), res[1]

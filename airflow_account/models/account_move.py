7ab4526b4f88740845dcdf09debe8743554754db
jefe-odoo <jefe@odoo.com>
2022-06-21 12:11:46 -0500

    [FIX]airflow_account: Fixes Issue 2 from task 2843969

--- airflow_account/models/account_move.py
+++ airflow_account/models/account_move.py
@@ -12,32 +12,29 @@ class AccountMove(models.Model):
         return order_id
 
     def post(self):
-
-        invoice = self._context.get('invoice', False)
-
-        if invoice:
-            if invoice.move_name and invoice.move_name != '/':
-                new_name = invoice.move_name
+        for invoice in self:
+            if invoice.name and invoice.name != '/':
+                new_name = invoice.name
             else:
                 # invoice has types:
                 # 'out_invoice' -> regular customer invoice
                 # 'out_refund' -> customer credit notes
                 new_name = ''
 
-                if invoice.type == 'out_invoice' and invoice.origin:
+                if invoice.type == 'out_invoice' and invoice.invoice_origin:
                     order_id = self._get_invoice_order(invoice)
                     if order_id:
                         order_id.write({'number_of_invoices': order_id.number_of_invoices + 1})
                         seq = order_id.number_of_invoices
-                        new_name = invoice.origin[2:] + '{0:02d}'.format(seq) if invoice.origin.startswith('SO') \
-                            else invoice.origin + '{0:02d}'.format(seq)
+                        new_name = invoice.invoice_origin[2:] + '{0:02d}'.format(seq) if invoice.invoice_origin.startswith('SO') \
+                            else invoice.invoice_origin + '{0:02d}'.format(seq)
 
-                if invoice.type == 'out_refund' and invoice.refund_invoice_id and invoice.refund_invoice_id.number:
-                    order_id = self._get_invoice_order(invoice.refund_invoice_id)
+                if invoice.type == 'out_refund' and invoice.reversed_entry_id and invoice.reversed_entry_id.name:
+                    order_id = self._get_invoice_order(invoice.reversed_entry_id)
                     if order_id:
                         order_id.write({'number_of_credit_notes': order_id.number_of_credit_notes + 1})
                         seq = order_id.number_of_credit_notes
-                        new_name = invoice.refund_invoice_id.number[:-2] + 'CM{0:01d}'.format(seq)
+                        new_name = invoice.reversed_entry_id.name[:-2] + 'CM{0:01d}'.format(seq)
 
                 if new_name:
                     # not sure why we need a loop here... just following the convention

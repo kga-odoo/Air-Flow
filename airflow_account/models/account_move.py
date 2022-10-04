83256fac661a40a70f8662bc84af14fd78a3d6a3
jefe-odoo <jefe@odoo.com>
2022-10-04 16:22:25 -0500

    [FIX]airflow_account: Fixes issue related to invoices name

--- airflow_account/models/account_move.py
+++ airflow_account/models/account_move.py
@@ -11,36 +11,25 @@ class AccountMove(models.Model):
         order_id = related_sols[0].order_id if related_sols else False
         return order_id
 
-    def post(self):
+    @api.depends('posted_before', 'state', 'journal_id', 'date')
+    def _compute_name(self):
+        super()._compute_name()
+        self._update_name_from_origin()
+
+    def _update_name_from_origin(self):
         for invoice in self:
-            if invoice.name and invoice.name != '/':
-                new_name = invoice.name
-            else:
-                # invoice has types:
-                # 'out_invoice' -> regular customer invoice
-                # 'out_refund' -> customer credit notes
-                new_name = ''
+            if invoice.move_type == 'out_invoice' and invoice.invoice_origin:
+                order_id = self._get_invoice_order(invoice)
+                if order_id:
+                    order_id.write({'number_of_invoices': order_id.number_of_invoices + 1})
+                    seq = order_id.number_of_invoices
+                    invoice.name = invoice.invoice_origin[2:] + '{0:02d}'.format(
+                        seq) if invoice.invoice_origin.startswith('SO') \
+                        else invoice.invoice_origin + '{0:02d}'.format(seq)
 
-                if invoice.type == 'out_invoice' and invoice.invoice_origin:
-                    order_id = self._get_invoice_order(invoice)
-                    if order_id:
-                        order_id.write({'number_of_invoices': order_id.number_of_invoices + 1})
-                        seq = order_id.number_of_invoices
-                        new_name = invoice.invoice_origin[2:] + '{0:02d}'.format(seq) if invoice.invoice_origin.startswith('SO') \
-                            else invoice.invoice_origin + '{0:02d}'.format(seq)
-
-                if invoice.type == 'out_refund' and invoice.reversed_entry_id and invoice.reversed_entry_id.name:
-                    order_id = self._get_invoice_order(invoice.reversed_entry_id)
-                    if order_id:
-                        order_id.write({'number_of_credit_notes': order_id.number_of_credit_notes + 1})
-                        seq = order_id.number_of_credit_notes
-                        new_name = invoice.reversed_entry_id.name[:-2] + 'CM{0:01d}'.format(seq)
-
-                if new_name:
-                    # not sure why we need a loop here... just following the convention
-                    for move in self:
-                        move.name = new_name
-
-        res = super(AccountMove, self).post()
-
-        return res
+            if invoice.move_type == 'out_refund' and invoice.reversed_entry_id and invoice.reversed_entry_id.name:
+                order_id = self._get_invoice_order(invoice.reversed_entry_id)
+                if order_id:
+                    order_id.write({'number_of_credit_notes': order_id.number_of_credit_notes + 1})
+                    seq = order_id.number_of_credit_notes
+                    invoice.name = invoice.reversed_entry_id.name[:-2] + 'CM{0:01d}'.format(seq)

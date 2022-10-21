4f599e27dc2f5c5f0ce1f6f6e38dcadeac5f67aa
jefe-odoo <jefe@odoo.com>
2022-10-21 12:35:23 -0500

    [IMP]airflow_account,airflow_stock: Attends feedback from 10/13/22 fist part

--- airflow_account/models/__init__.py
+++ airflow_account/models/__init__.py
@@ -1,10 +1,7 @@
-# -*- coding: utf-8 -*
-
 from . import sale_order
 from . import account_move
 from . import account_payment_term
 from . import account_followup_report
-from . import account_invoice
 from . import account_payment
 from . import sales_team
 from . import account_move_reversal

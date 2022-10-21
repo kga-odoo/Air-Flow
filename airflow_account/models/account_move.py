110be9bd8869129b02c98a0f534c62d908108379
jefe-odoo <jefe@odoo.com>
2022-10-21 12:31:00 -0500

    [IMP]airflow_account,airflow_stock: Attends feedback from 10/13/22

--- airflow_account/models/account_move.py
+++ airflow_account/models/account_move.py
@@ -1,10 +1,43 @@
-# -*- coding: utf-8 -*-
 from odoo import models, fields, api, _
-from odoo.exceptions import ValidationError
 
 
 class AccountMove(models.Model):
     _inherit = 'account.move'
+
+    paid_date = fields.Date(string='Paid Date')
+    sale_id = fields.Many2one('sale.order', compute="_compute_sale_id", string="Original Sale Order", store=True)
+
+    # The field project_manager is depricated. Airflow wishes to have it hold Char type instead of many2one.
+    # Keeping field for historical reasons and data preservation.
+    proj_manager = fields.Many2one('res.partner', related='sale_id.proj_manager', string="Project Manager(Depricated)",
+                                   store=True)
+
+    proj_manager_name = fields.Char(related='sale_id.proj_manager_name', string="Project Manager", store=True)
+    proj_name = fields.Char(related='sale_id.proj_name', string="Project Name", store=True)
+
+    ship_date = fields.Date(related='sale_id.ship_date', string="Ship Date", store=True)
+    ship_method = fields.Many2one('delivery.carrier', related='sale_id.ship_method', string="Shipping Method",
+                                  store=True)
+
+    @api.depends('invoice_line_ids', 'invoice_line_ids.sale_line_ids', 'invoice_line_ids.sale_line_ids.order_id')
+    def _compute_sale_id(self):
+        for account in self:
+            # Handle case of credit note
+            # [MIG] now handled in reversal wizard
+            # if account.type == 'out_refund':
+            #     account.sale_id = account.refund_invoice_id.sale_id.id if account.refund_invoice_id.sale_id.id else False
+            # Handle case of cust invoice
+            if account.move_type == 'out_invoice':
+                sale_ids = account.mapped('invoice_line_ids.sale_line_ids.order_id')
+                account.sale_id = sale_ids[0] if sale_ids else False
+            else:
+                account.sale_id = False
+
+    def write(self, vals):
+        """Inherit to set the paid date"""
+        if vals.get('state') == 'paid':
+            vals['paid_date'] = fields.Date.context_today(self)
+        return super(AccountMove, self).write(vals)
 
     def _get_invoice_order(self, invoice_id):
         related_sols = invoice_id.invoice_line_ids.mapped('sale_line_ids')
